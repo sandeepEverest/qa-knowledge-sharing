e2831711be8a12a2e60d5037a0472fcf52a01594
deekshithdev1992 <159556528+deekshithdev1992@users.noreply.github.com>
2024-07-31 20:54:17 +0530

    learners

--- js-class.js
+++ js-class.js
@@ -6,6 +6,14 @@
 
 // adding third commit
 
-adding 4th commit
+//adding 4th commit
 
 console.log("JavaScript")
+
+let learner1="Ravi";
+let learner2="Pavan";
+let learner3="Deekshith";
+
+console.log(learner1)
+console.log(learner2)
+console.log(learner3)
