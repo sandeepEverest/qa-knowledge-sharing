8ee0dd4a431e5fa28252822da7b19b3097f65f6c
PavanKumar Rao <pavankumarrao@PavanKumars-MacBook-Pro.local>
2024-08-25 19:00:27 +0530

    basic js datatypes

---
+++ basics.js
@@ -0,0 +1,18 @@
+let a = 4;
+console.log("To print output in JS");
+console.log(a);
+console.log(typeof a);
+// Assign decimal values to variable
+let b = 155.89;
+console.log(b);
+console.log(typeof b);
+//Assign string to variable
+var c = "Pavan";
+console.log(typeof c);
+//Adding a and b values
+// let c=a+b  we can't redclare variables with let type
+c = a + b; // we can recreate a variable with adding varible type
+console.log(c);
+var c = a + b; //by using var we can recreate and redeclare the variables
+console.log(c);
+console.log(typeof c);
