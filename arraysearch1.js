3ad680b0dd5a203cd14093b8b8821dc5d8463a34
Ravi Shankar <ravishankar@Ravis-MacBook-Pro.local>
2024-09-04 10:57:19 +0530

    arrays

---
+++ arraysearch1.js
@@ -0,0 +1,14 @@
+let cars=["audi", "toyota", "bmw",'audi', 'audi']
+console.log(cars)
+let position
+position=cars.indexOf('toyota')
+last=cars.lastIndexOf('audi')
+console.log("the position of the element is " +position)
+console.log("the last occurence of the car Audi is " +last)
+console.log(cars.includes("nokia"))
+/*console.log(cars[3])
+let car=cars[2]
+console.log(car)
+cars[4]="test"
+console.log(cars) */
+
